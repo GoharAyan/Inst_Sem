d64e72b3e4d2a0618d1ede4b36b9b62c8938b9f7
GoharAyan <gohar.adamyan.im@gmail.com>
2022-09-01 17:14:47 +0400

    Remove

--- create_file_with_index.sh
+++
@@ -1,7 +0,0 @@
-#!usr/bin/bash
-
-for i in {1..10}
-do
-    touch file_${i}.txt
-    echo $i >> file_${i}.txt
-done
--- hierarchy_with_ifelse.sh
+++
@@ -1,9 +0,0 @@
-#! usr/bin/bash
-
-if [ -f A/B/C ]
-then
-    echo A/B/C folders exist!
-else
-    mkdir -p A/B/C
-    cd A/B/C
-fi
